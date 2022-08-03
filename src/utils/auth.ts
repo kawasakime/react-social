e6888209e0cfeb8f5840558458faa43bf1f202f3
wyylame <w1llhorn@yandex.ru>
2022-08-03 16:31:52 +0300

    added login user utils

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,18 +1,26 @@
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+const auth = getAuth();
 
 export const registerUser = async (email: string, password: string) => {
-  const auth = await getAuth();
-  return await createUserWithEmailAndPassword(auth, email, password)
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const loginUser = async (email: string, password: string) => {
+  return await signInWithEmailAndPassword(auth, email, password)
 }
 
-
-  // .then((userCredential) => {
-  //   // Signed in 
-  //   const user = userCredential.user;
-  //   // ...
-  // })
-  // .catch((error) => {
-  //   const errorCode = error.code;
-  //   const errorMessage = error.message;
-  //   // ..
-  // });
+// .then((userCredential) => {
+//   // Signed in
+//   const user = userCredential.user;
+//   // ...
+// })
+// .catch((error) => {
+//   const errorCode = error.code;
+//   const errorMessage = error.message;
+//   // ..
+// });
