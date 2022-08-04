9fc5a0c9914e2821b37614ca8e4ebec8082c50ea
wyylame <w1llhorn@yandex.ru>
2022-08-04 20:21:12 +0300

    added name processing during registration

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import * as React from "react";
 
 import { TbArrowBackUp } from "react-icons/tb";
@@ -8,37 +8,39 @@ import Loader from "../components/UI/Loader";
 import Success from "../components/UI/Success";
 import { auth } from "../firebase";
 
-type Error = { status: boolean; message: string };
+export type AuthError = { status: boolean; message: string };
 
 interface IRegistrationProps {}
 
 const Registration: React.FunctionComponent<IRegistrationProps> = (props) => {
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
   const [isSuccess, setIsSucces] = React.useState<boolean>(false);
-  const [error, setError] = React.useState<Error>({
+  const [error, setError] = React.useState<AuthError>({
     status: false,
     message: "Ошибка",
   });
 
   const navigate = useNavigate();
 
-  // console.log(auth.currentUser?.getIdToken)
+  console.log(auth.currentUser);
 
   function handleRegisterUser(
     e: React.MouseEvent<HTMLButtonElement>,
     email: string,
-    password: string
+    password: string,
+    name: string
   ) {
     e.preventDefault();
     setIsLoading(true);
     error.status = false;
     createUserWithEmailAndPassword(auth, email, password)
-      .then(({ user }) => {
+      .then(({ user }) => updateProfile(user, { displayName: name }))
+      .then(() => {
         setIsSucces(true);
         setTimeout(() => {
           setIsSucces(false);
-          navigate('/');
-        }, 2000);
+          navigate("/");
+        }, 1500);
       })
       .catch((error) => {
         setError({ status: true, message: error.code });
