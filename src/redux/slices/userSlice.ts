f4a14841ff722e91e94afa4a9a12f7c7f8649ad5
wyylame <w1llhorn@yandex.ru>
2022-08-04 20:22:07 +0300

    added name property to the state

--- src/redux/slices/userSlice.ts
+++ src/redux/slices/userSlice.ts
@@ -3,13 +3,15 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 export interface UserState {
     email: string | null,
     token: string | null,
-    id: string | null
+    id: string | null,
+    name: string | null
 }
 
 const initialState: UserState = {
     email: null,
     token: null,
-    id: null
+    id: null,
+    name: null
 }
 
 const userSlice = createSlice({
@@ -20,11 +22,13 @@ const userSlice = createSlice({
       state.email = action.payload.email
       state.token = action.payload.token
       state.id = action.payload.id
+      state.name = action.payload.name
     },
     removeUser: (state) => {
       state.email = null;
       state.token = null;
       state.id = null;
+      state.name = null
     }
   }
 })
