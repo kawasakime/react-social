9f9fbe1f0153ef450238053b3384f99165cf7715
wyylame <w1llhorn@yandex.ru>
2022-08-04 21:29:17 +0300

    added error and wrapper styles

--- src/@types/custom.d.ts
+++ src/@types/custom.d.ts
@@ -2,3 +2,8 @@ declare module "*.scss" {
   const styles: { [className: string]: string };
   export default styles;
 }
+
+declare module "*.png" {
+  const image: string;
+  export default image;
+}
